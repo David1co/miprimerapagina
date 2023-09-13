e1fb1618e203b473a49806a47456b7650731002e
David1co <cdlopezc11@gmail.com>
2023-09-12 20:34:41 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//alert("Hola este es mi javascript")
+
+//let nombre = "Marta";
+//nombre = "Maria";
+
+//var nombre1 = "Marta";
+
+//const nombre2 = "Marta";
+// //nombre2 = "Maria";
+
+//console.log(nombre);
+//console.log(nombre1);
+//console.log(nombre2);
